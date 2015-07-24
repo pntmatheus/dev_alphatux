437258e71c44adbe294869376899f8ca8a13f5be
pntmatheus <pnt.matheus@gmail.com>
2015-07-24 12:26:18 -0300

    View puxando objetos

--- core/views.py
+++ core/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
 
+from .models import Pessoa
+
+"""
 def index(request):
     return HttpResponse("Olá Mundo!!!")
+"""
+
+def index(request):
+    teste = Pessoa.objects.all()
+    output = ', '.join([p.nome for p in teste])
+    return HttpResponse(output)
