bcfa5650ebd2d32b5470f957c09ffb6123e03d5d
pntmatheus <pnt.matheus@gmail.com>
2015-07-28 17:44:04 -0300

    Paramiko

--- core/views.py
+++ core/views.py
@@ -2,12 +2,26 @@ from django.http import HttpResponse
 
 from .models import Pessoa
 
+import paramiko
+
 """
 def index(request):
     return HttpResponse("Olá Mundo!!!")
 """
 
 def index(request):
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect('192.168.1.13', username='admin', password='admin')
+
+    stdin,stdout,stderr = ssh.exec_command('ip address print')
+
     teste = Pessoa.objects.all()
-    output = ', '.join([p.nome for p in teste])
+    #output = ', '.join([p.nome for p in teste])
+
+    output = '<br>'.join([p for p in stdout])
+
+    #output = stderr.read()
+    #output = stdout.readlines()
     return HttpResponse(output)
