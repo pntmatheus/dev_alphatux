1c68b9ba8d4786fd629fc9ab77c1ae92812c5417
pntmatheus <pnt.matheus@gmail.com>
2015-07-28 18:20:47 -0300

    DSA Public-key

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.http import HttpResponse
 
 from .models import Pessoa
@@ -11,14 +13,13 @@ def index(request):
 
 def index(request):
 
+    path = os.path.join(os.environ['HOME'], '.ssh', 'id_dsa')
+    pkey = paramiko.DSSKey.from_private_key_file(path, password=None)
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('192.168.1.13', username='admin', password='admin')
+    ssh.connect('192.168.1.13', username='admin-ssh', pkey=pkey)
 
     stdin,stdout,stderr = ssh.exec_command('ip address print')
-
-    teste = Pessoa.objects.all()
-    #output = ', '.join([p.nome for p in teste])
 
     output = '<br>'.join([p for p in stdout])
 
