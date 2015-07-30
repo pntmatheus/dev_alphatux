1b45e8eef8d5204ccd95ff6d3bb4b9641199be57
pntmatheus <pnt.matheus@gmail.com>
2015-07-30 17:58:53 -0300

    trabalhando com EXCEL

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 
 from .models import Pessoa
 
+from openpyxl import load_workbook
 import paramiko
 
 """
@@ -17,12 +18,21 @@ def index(request):
     pkey = paramiko.DSSKey.from_private_key_file(path, password=None)
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('192.168.1.13', username='admin-ssh', pkey=pkey)
+    ssh.connect('192.168.1.13', username='teste', pkey=pkey)
+
+    ssh.exec_command('ip address add address=10.1.1.1/24 interface=ether1')
 
     stdin,stdout,stderr = ssh.exec_command('ip address print')
+
+    wb = load_workbook(filename='clientesTerezinha.xlsx', read_only=True)
+    ws = wb['Plan1']
+
+    #teste = ws['A67'].value
+
+    teste = ''.join()
 
     output = '<br>'.join([p for p in stdout])
 
     #output = stderr.read()
     #output = stdout.readlines()
-    return HttpResponse(output)
+    return HttpResponse(teste)
